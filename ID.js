2682e6da034e8e3a327dec8767f735032cbde2c7
eR1zxNO <31590640+eR1zxNO@users.noreply.github.com>
2019-11-29 09:36:52 +0800

    Update ID.js

--- ID.js
+++ ID.js
@@ -1,3 +1,12 @@
+/ *
+Quantumult X 1.0.3 加ID
+
+QX 1.0.3：
+^  ^https:\/\/(raw.githubusercontent|\w+\.github)\.(com|io)\/.*\.js$ url script-response-body https://raw.githubusercontent.com/eR1zxNO/Rewrite/master/ID.js
+
+MitM = raw.githubusercontent.com, *.github.io, 
+* /
+
 var body = $response.body;
 body = '\/*\n@supported 8CAAE3BAA2B1\n*\/\n' + body;
 
